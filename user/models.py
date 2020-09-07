3ef1338c885c310dbaf8ff396fa8df827579e91d
Che Sampat <chesampat@gmail.com>
2020-09-07 17:24:34 +0100

    Removed steam_id method from User and added signal to auto-populate it from the SocialAccount model

--- user/models.py
+++ user/models.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from allauth.socialaccount.models import SocialAccount
+
 
 import uuid
 
@@ -9,13 +12,19 @@ import uuid
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
+    steam_id = models.CharField(unique=True, max_length=100, db_index=True, null=True, blank=True)
+
     def __str__(self):
         return str(self.id)
     
     @property
     def steam_account(self):
         return SocialAccount.objects.get(user=self)
+        
 
-    @property
-    def steam_id(self):
-        return self.steam_account.extra_data['steamid']
+@receiver(post_save, sender=SocialAccount)
+def save_steam_id_to_user(sender, instance, created, **kwargs):
+    if created:
+        # Saves the Steam ID to the User model so it can be queried directly 
+        instance.user.steam_id = instance.extra_data['steamid']
+        instance.user.save()
