9aafe5763b53023845b2cf844c8e2869c9f2c3d0
Che Sampat <chesampat@gmail.com>
2020-09-10 16:31:14 +0100

    changed urls

--- ticket/urls.py
+++ ticket/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from ticket import views
 
 urlpatterns = [
-    path('ticket/validate', views.validate),
-    path('ticket/generate', views.generate)
+    path('validate/', views.validate),
+    path('generate/', views.generate)
 ]
