f002a3f28182d52be1f99b03fdda6a68f8e718a7
Che Sampat <chesampat@gmail.com>
2020-09-11 22:14:16 +0100

    removed unused import

--- user/models.py
+++ user/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from social_django.models import AbstractUserSocialAuth
 from rest_framework.authtoken.models import Token
 
 import uuid
